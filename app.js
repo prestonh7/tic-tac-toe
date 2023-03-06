f90bb18a2e6faaab49f647c5b3ce884649b34ef5
prestonh7 <prestonhouse7@gmail.com>
2023-03-06 17:09:49 -0600

    Gameboard gets drawn

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const gameboard = (() => {
   const board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
   ];
+  // Draws gameboard to screen
+  const content = document.querySelector('.content');
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const button = document.createElement('button');
+      button.className = `gameSquare row-${i} column-${j}`;
+      content.appendChild(button);
+    }
+  }
   function resetBoard() {
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
@@ -17,12 +26,15 @@ const gameboard = (() => {
   return { resetBoard, addPiece, board }; // Prob remove board
 })();
 
-const Player = (piece) => {
-  this.piece = piece;
-  function placePiece() {
-    gameboard.addPiece(this.piece, row, column);
-  }
-  return { piece };
-};
+const gameState = (() => {
+  const Player = (piece) => {
+    this.piece = piece;
+    function placePiece() {
+      gameboard.addPiece(this.piece, row, column);
+    }
+    return { piece };
+  };
 
-const playerOne = Player(1);
+  const playerOne = Player('x');
+  const playerTwo = Player('o');
+})();
