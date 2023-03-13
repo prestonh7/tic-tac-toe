ff7d79dc9fb9805ff4f1ef7ef8a4d8ae5e437e99
prestonh7 <prestonhouse7@gmail.com>
2023-03-13 18:45:13 -0500

    Move functions around, added resetGame

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const gameState = (() => {
-  const turn = 1;
+  let turn = 1;
+  function getTurn() {
+    return turn;
+  }
   function changeTurn() {
-    if (this.turn === 1) {
-      this.turn = 2;
+    if (turn === 1) {
+      turn = 2;
     } else {
-      this.turn = 1;
+      turn = 1;
     }
   }
-  return { changeTurn };
+  function resetTurn() {
+    turn = 1;
+  }
+  return { changeTurn, resetTurn, getTurn };
 })();
 
 const Player = (piece) => ({
@@ -44,44 +50,41 @@ const gameboard = (() => {
     // Check rows
     for (let i = 0; i < board.length; i++) {
       if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-        resetBoard();
-        removePieces();
         return true;
       }
     }
     // Check columns
     for (let j = 0; j < board[0].length; j++) {
       if (board[0][j] !== '' && board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
-        resetBoard();
-        removePieces();
         return true;
       }
     }
     // Check diagonals
     if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-      resetBoard();
-      removePieces();
       return true;
     }
     if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-      resetBoard();
-      removePieces();
       return true;
     }
     // No winner found
     return false;
+  }
+  function resetGame() {
+    resetBoard();
+    removePieces();
+    gameState.resetTurn();
   }
   // Adds piece to array
   function addPiece(piece, row, column) {
     board[row][column] = piece;
   }
   // Checks who's turn it is and adds the piece
-  function playTurn(i, j) {
-    if (gameState.turn === 1) {
-      addPiece(playerOne.piece, i, j);
+  function playTurn(row, column) {
+    if (gameState.getTurn() === 1) {
+      addPiece(playerOne.piece, row, column);
       gameState.changeTurn();
     } else {
-      addPiece(playerTwo.piece, i, j);
+      addPiece(playerTwo.piece, row, column);
       gameState.changeTurn();
     }
   }
@@ -94,19 +97,25 @@ const gameboard = (() => {
       // Uses for loop variables to select correct element
       button.addEventListener('click', (e) => {
         if (validTurn(i, j) === true) {
-          if (gameState.turn === 1) {
+          if (gameState.getTurn() === 1) {
             playTurn(i, j);
             button.innerText = 'X';
-            checkForWinner();
-          } else {
+            if (checkForWinner()) {
+              resetGame();
+              console.log("X's wins!");
+            }
+          } else if (gameState.getTurn() === 2) {
             playTurn(i, j);
             button.innerText = 'O';
-            checkForWinner();
+            if (checkForWinner()) {
+              resetGame();
+              console.log("O's wins!");
+            }
           }
         }
       });
       content.appendChild(button);
     }
   }
-  return { resetBoard, board }; // Prob remove board
+  return { resetBoard, resetGame, board }; // Prob remove board
 })();
